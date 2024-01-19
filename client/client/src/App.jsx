0f4473212cac2f31cf77ad81f1007a5a5c4d62df
unknown <ishetuh>
2024-01-19 15:34:17 +0300

    create pages

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from "react";
-
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import Signing from "./pages/Signing";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className='text-3xl text-red-500'>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={Home} />
+          <Route path='/About' element={About} />
+          <Route path='/Projects' element={Projects} />
+          <Route path='/Signing' element={Signing} />
+          <Route path='/SignUp' element={SignUp} />
+          <Route path='/Dashboard' element={Dashboard} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
